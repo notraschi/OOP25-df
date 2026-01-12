dcb0b9e90fc47d7395ffcfbc93389bfad6573dd7
tedesco-lorenzo <lorenzo.tedesco2@studio.unibo.it>
2026-01-12 19:36:32 +0100

    Add ability combination logic using a map of valid ability pairs

---
+++ src/main/java/it/unibo/df/model/abilities/combine/DefaultCombinations.java
@@ -0,0 +1,58 @@
+package it.unibo.df.model.abilities.combine;
+
+import it.unibo.df.model.abilities.Ability;
+import it.unibo.df.model.abilities.AbilityType;
+
+/**
+ * Default combinations.
+ */
+public final class DefaultCombinations {
+
+    private DefaultCombinations() { }
+
+    /**
+     * Creates a combiner with default combos.
+     *
+     * @return combiner
+     */
+    public static AbilityCombiner create() {
+        final AbilityCombiner c = new AbilityCombiner();
+
+        // Ex: 1 + 2 -> new ability (id 100)
+        c.register(1, 2, (a, b) -> buildCombined(100, a, b));
+
+
+        return c;
+    }
+
+    /**
+     * Builds a combined ability.
+     *
+     * @param newId id of result
+     * @param a first ability
+     * @param b second ability
+     * @return combined ability
+     */
+    private static Ability buildCombined(final int newId, final Ability a, final Ability b) {
+        return new Ability(
+            newId,
+            a.name() + "+" + b.name(),
+            a.cooldown() + b.cooldown(),
+            pickType(a, b),
+            a.casterHpDelta() + b.casterHpDelta(),
+            a.targetHpDelta() + b.targetHpDelta(),
+            a.effect()
+        );
+    }
+
+    private static AbilityType pickType(final Ability a, final Ability b) {
+        if (a.type() == AbilityType.LIFESTEAL || b.type() == AbilityType.LIFESTEAL) {
+            return AbilityType.LIFESTEAL;
+        }
+        if (a.type() == AbilityType.HEAL && b.type() == AbilityType.HEAL) {
+            return AbilityType.HEAL;
+        }
+        return AbilityType.ATTACK;
+    }
+}
+
