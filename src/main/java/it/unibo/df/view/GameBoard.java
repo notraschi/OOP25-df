f05cc06098cd985c9e392669cac772ff9b2645c3
Riccardo Ragazzini <riccardo.ragazzini4@studio.unibo.it>
2026-02-14 17:53:44 +0100

    view: fixed method names

--- src/main/java/it/unibo/df/view/GameBoard.java
+++ src/main/java/it/unibo/df/view/GameBoard.java
@@ -64,7 +64,7 @@ public class GameBoard {
         formatColumns(playArea, Constants.BOARD_SIZE, MAX_SIZE_PERC / Constants.BOARD_SIZE);
         formatRows(playArea, Constants.BOARD_SIZE, MAX_SIZE_PERC / Constants.BOARD_SIZE);
         centerPane.add(fillPlayArea(playArea), 0, 0); 
-        centerPane.add(fillLowBar(), 0, 1);
+        centerPane.add(fillLowBarArea(), 0, 1);
         final SceneResizer resizer = new SceneResizer(
             centerPane,
             Double.valueOf(BOARD_SIZE_PERC) / Double.valueOf(MAX_SIZE_PERC),
@@ -131,7 +131,7 @@ public class GameBoard {
         return area;
     }
 
-    private GridPane fillLowBar() {
+    private GridPane fillLowBarArea() {
         final GridPane lowBar = new GridPane();
         final int nColumns = 3;
         formatColumns(lowBar, nColumns, MAX_SIZE_PERC / nColumns);
@@ -193,7 +193,7 @@ public class GameBoard {
      * 
      * @param equipment list of abilities equipped
      */
-    public void refreshAbility(final List<AbilityView> equipment) {
+    public void refreshAbilities(final List<AbilityView> equipment) {
         equipped = equipment;
         final Iterator<AbilityView> equipIt = equipment.iterator();
         for (final var e : abilityArea.getChildren()) {
